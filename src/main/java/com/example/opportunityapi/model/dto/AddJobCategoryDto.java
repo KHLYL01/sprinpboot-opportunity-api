ade0ef26b4e9e3c18450551dd49ef09ba1099368
KHLYL01 <kk0026999@gmail.com>
2024-07-24 19:54:24 +0300

    fix add jop category dto

--- src/main/java/com/example/opportunityapi/model/dto/AddJobCategoryDto.java
+++ src/main/java/com/example/opportunityapi/model/dto/AddJobCategoryDto.java
@@ -1,8 +1,6 @@
 package com.example.opportunityapi.model.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -10,6 +8,8 @@ import java.time.LocalDateTime;
 @Setter
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class AddJobCategoryDto {
 
